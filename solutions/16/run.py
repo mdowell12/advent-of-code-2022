66158efe2856b1c707f17a6491f40a31184edd02
Matt Dowell <matt.dowell@sproutsocial.com>
2022-12-16 15:38:17 -0800

    Day 16 part 2

--- solutions/16/run.py
+++ solutions/16/run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from collections import defaultdict
 from itertools import product, combinations
 
@@ -10,7 +11,7 @@ def run_1(inputs):
     valve_name_to_valve = {v[0]: v for v in valves}
     starting_valve = [v for v in valves if v[0] == 'AA'][0]
     max_at_valve = defaultdict(lambda: 0)
-    
+
     paths = [Path(starting_valve)]
     for i in range(30):
         next_paths = []
@@ -28,7 +29,6 @@ def run_1(inputs):
                 next_paths.append(path.open_current_valve())
         paths = [p for p in next_paths]
         print(f'min {i} with {len(paths)} possible paths')
-        
 
     best = sorted(paths, key=lambda p: p.pressure_released)[-1]
     return best.pressure_released
@@ -36,42 +36,59 @@ def run_1(inputs):
 
 def run_2(inputs):
     valves = parse_inputs(inputs)
+    valves_with_nonzero_flow = [v for v in valves if v[1] != 0]
     valve_to_pressure = {v[0]: v[1] for v in valves}
     valve_name_to_valve = {v[0]: v for v in valves}
+    total_release = sum(valve_name_to_valve[v[0]][1] for v in valves_with_nonzero_flow)
     starting_valve = [v for v in valves if v[0] == 'AA'][0]
     max_at_valve = defaultdict(lambda: 0)
-    
+
+    best_score = 0
+
     paths = [Path(starting_valve, elephant_last_valve=starting_valve)]
     for i in range(26):
         next_paths = []
+        path_start = datetime.now()
         for path in paths:
+
+            if path.all_valves_open(valves_with_nonzero_flow):
+                final_score = calc_score_for_finished_path(path, i, 26, total_release)
+                max_at_valve[path.names_and_opened()] = final_score
+                if final_score > best_score:
+                    best_score = final_score
+                continue
+
             path.release_pressure(valve_to_pressure)
-            # if path.all_valves_open(valves):
-            #     next_paths.append(path)
-            #     continue
+
             neighbors = path.neighbors(valve_name_to_valve)
-            # import pdb; pdb.set_trace()
             for neighbor in neighbors:
-                last_valve_name = neighbor.last_valve[0]
-                elephant_last_valve_name = neighbor.elephant_last_valve[0]
-                if neighbor.pressure_released >= max_at_valve[','.join(sorted([last_valve_name, elephant_last_valve_name]))]:
-                    if not path.all_valves_open(valves):
-                        max_at_valve[','.join(sorted([last_valve_name, elephant_last_valve_name]))] = neighbor.pressure_released
-                        next_paths.append(neighbor)
-                # else:
-                #     print('visted')
-                # # if neighbor.pressure_released >= max_at_valve[last_valve_name] >= max_at_valve[path.last_valve[0]]:
+                if neighbor.pressure_released >= max_at_valve[neighbor.names_and_opened()]:
+                    max_at_valve[neighbor.names_and_opened()] = neighbor.pressure_released
+                    next_paths.append(neighbor)
+        path_end = datetime.now()
 
-            # if path.current_valve_is_closed() and path.elephant_valve_is_closed():
-            #     next_paths.append(path.open_current_valve().open_elephant_valve())
-            # if path.elephant_valve_is_closed():
-            #     next_paths.append(path.open_elephant_valve())
-        paths = [p for p in next_paths]
-        print(f'min {i} with {len(paths)} possible paths')
-        
+        dedupe_start = datetime.now()
+        next_paths_deduped = {}
+        for path in next_paths:
+            if path.names_and_opened() not in next_paths_deduped or path.pressure_released > next_paths_deduped[path.names_and_opened()].pressure_released:
+                next_paths_deduped[path.names_and_opened()] = path
+        paths = [path for path in sorted([p for p in next_paths_deduped.values()], key=lambda p: p.pressure_released)][-100000:]
 
-    best = sorted(paths, key=lambda p: p.pressure_released)[-1]
-    return best.pressure_released
+        dedupe_end = datetime.now()
+        path_elapsed = (path_end-path_start).seconds
+        dedupe_elapsed = (dedupe_end-dedupe_start).seconds
+        print(f'min {i+1} with {len(paths)} paths deduped from a possible {len(next_paths)}. Path took {path_elapsed} dedupe took {dedupe_elapsed}')
+
+        if paths[-1].pressure_released > best_score:
+            best_score = paths[-1].pressure_released
+
+    return best_score
+
+
+def calc_score_for_finished_path(finished_path, current_minute, total_minutes, total_release_on_map):
+    minutes_remaining = total_minutes - current_minute
+    return finished_path.pressure_released + minutes_remaining * total_release_on_map
+
 
 
 class Path:
@@ -109,31 +126,37 @@ class Path:
         copy.opened_valves.add(copy.elephant_last_valve[0])
         return copy
 
+    def names(self):
+        return f'{self.last_valve[0]},{self.elephant_last_valve[0]}'
+
+    def names_and_opened(self):
+        opened = ','.join(sorted([i for i in self.opened_valves]))
+        return f'{self.last_valve[0]},{self.elephant_last_valve[0]},{opened}'
+
     def neighbors(self, valve_name_to_valve):
         result = []
-        
+
         if self.elephant_last_valve is None:
             for valve_name in self.last_valve[2]:
                 valve = valve_name_to_valve[valve_name]
                 result.append(self.copy(last_valve=valve))
         else:
-            # import pdb; pdb.set_trace()
-            valve_name_combos = combinations(set(self.last_valve[2]).union(self.elephant_last_valve[2]), 2)
-            # valve_name_combos = product(self.last_valve[2], self.elephant_last_valve[2])
+            valve_name_combos = product(self.last_valve[2], self.elephant_last_valve[2]) if self.last_valve[0] != self.elephant_last_valve[0] else combinations(set(self.last_valve[2]).union(self.elephant_last_valve[2]), 2)
             for valve_name, elephant_valve_name in valve_name_combos:
                 valve = valve_name_to_valve[valve_name]
                 elephant_valve = valve_name_to_valve[elephant_valve_name]
-                result.append(self.copy(last_valve=valve, elephant_last_valve=elephant_valve))
+                copy = self.copy(last_valve=valve, elephant_last_valve=elephant_valve)
+                result.append(copy)
 
-            if self.current_valve_is_closed():
+            if self.current_valve_is_closed() and self.last_valve[1] != 0:
                 with_current_opened = self.open_current_valve()
                 for valve_name in with_current_opened.elephant_last_valve[2]:
                     valve = valve_name_to_valve[valve_name]
                     result.append(with_current_opened.copy(elephant_last_valve=valve))
-                if with_current_opened.elephant_valve_is_closed():
-                    with_elephant_opened = with_current_opened.open_current_valve()
+                if with_current_opened.elephant_valve_is_closed() and with_current_opened.elephant_last_valve[1] != 0:
+                    with_elephant_opened = with_current_opened.open_elephant_valve()
                     result.append(with_elephant_opened)
-            elif self.elephant_valve_is_closed():
+            if self.elephant_valve_is_closed() and self.elephant_last_valve[1] != 0:
                 with_elephant_opened = self.open_elephant_valve()
                 for valve_name in with_elephant_opened.last_valve[2]:
                     valve = valve_name_to_valve[valve_name]
@@ -145,13 +168,14 @@ class Path:
         elephant_last_valve = self.elephant_last_valve if elephant_last_valve is None else elephant_last_valve
         return Path(
             last_valve,
-            opened_valves=set(i for i in self.opened_valves), 
-            pressure_released=self.pressure_released, 
+            opened_valves=set(i for i in self.opened_valves),
+            pressure_released=self.pressure_released,
             elephant_last_valve=elephant_last_valve
         )
 
     def __repr__(self):
-        return f'me={self.last_valve[0]} elephant={self.elephant_last_valve[0]} opened={self.opened_valves} released={self.pressure_released}'
+        opened = sorted([i for i in self.opened_valves])
+        return f'me={self.last_valve[0]} elephant={self.elephant_last_valve[0]} opened={opened} released={self.pressure_released}'
 
 
 def parse_inputs(inputs):
