89026146ebacc47372f3906c0190238442d67379
Matt Dowell <mattdowell12@gmail.com>
2022-12-18 20:31:10 -0800

    Day 17 part 1

--- solutions/17/run.py
+++ solutions/17/run.py
@@ -6,116 +6,179 @@ from solutions.get_inputs import read_inputs
 ROCKS = [
     # Horizontal line
     {
-        0: [0, 1, 2, 3],
-        1: [],
-        2: [],
-        3: [],
+        0: [0],
+        1: [0],
+        2: [0],
+        3: [0],
     },
     # Cross
     {
         0: [1],
-        1: [0, 1, 2],
+        1: [0, 2],
         2: [1],
-        3: [],
+        3: None,
     },
     # El
     {
-        0: [0, 1, 2],
-        1: [2],
-        2: [2],
-        3: [],
+        0: [0],
+        1: [0],
+        2: [0, 1, 2],
+        3: None,
     },
     # Vertical line
     {
-        0: [1],
-        1: [1],
-        2: [1],
-        3: [1],
+        0: [0, 1, 2, 3],
+        1: None,
+        2: None,
+        3: None,
     },
     # Square
     {
         0: [0, 1],
         1: [0, 1],
-        2: [],
-        3: [],
+        2: None,
+        3: None,
     },
 
 ]
 
 
 def run_1(inputs):
-    gas_directions = cycle(list(inputs[0].strip()))
+    return run(inputs, 2022)
+
+
+def run_2(inputs):
+    return run(inputs, 1_000_000)
+
+
+def run(inputs, num_rocks):
+    gas_str = list(inputs[0].strip())
+    gas_directions = cycle(gas_str)
     width = 7
-    slot_to_height = {i: -1 for i in range(width)}
+    slot_heights = {i: [-1] for i in range(width)}
     max_height = 0
+    num_gas_changes = 0
     rocks = cycle(ROCKS)
 
-    for i in range(2022):
+    for i in range(num_rocks):
+        if i % len(ROCKS) == 0 and num_gas_changes % len(gas_str) == 0 and all(max(slot_heights[i]) == max_height for i in slot_heights):
+            import pdb; pdb.set_trace()
+        if i % len(ROCKS) == 0 and num_gas_changes % len(gas_str) == 0 :
+            import pdb; pdb.set_trace()
         rock = rocks.__next__()
-        slot_to_height, gas_directions = do_rock(rock, slot_to_height, gas_directions, max_height)
-        max_height = max(v for v in slot_to_height.values())
+        slot_heights, gas_directions, num_gas_changes = do_rock(rock, slot_heights, gas_directions, max_height)
+        max_height = max(max(v) for v in slot_heights.values())
         print(i)
+        # print([sorted(slot_to_height[i]) for i in sorted(slot_to_height)])
+        # import pdb; pdb.set_trace()
+    # print(slot_to_height)
+    return max_height+1
 
-    return max_height
 
-
-def do_rock(rock, slot_to_height, gas_directions, max_height):
-    rock_positions = starting_rock_position(rock, max_height, slot_to_height)
+def do_rock(rock, slot_heights, gas_directions, max_height):
+    rock_positions = starting_rock_position(rock, max_height, slot_heights)
     down = False
-    left = min(i for i in rock_positions if rock_positions[i] is not None)
+    num_gas_changes = 0
     
     while True:
-        # import pdb; pdb.set_trace()
         # move rock
         if down:
-            # import pdb; pdb.set_trace()
-            next_rock_positions = {i: rock_positions[i] - 1 if rock_positions[i] is not None else None for i in slot_to_height}
-            if any(height == slot_to_height[i] for i, height in next_rock_positions.items()):
+            next_rock_positions = next_positions_down(rock_positions)
+            if intersects_with_other_rock(slot_heights, next_rock_positions):
                 break
             else:
                 rock_positions = next_rock_positions
         else:
-            
             gas_direction = 1 if gas_directions.__next__() == '>' else -1
-            # if gas_direction != 1:
-            #     import pdb; pdb.set_trace()
-            # next_rock_positions = {i: rock_positions.get(i-gas_direction) for i in slot_to_height}
-            next_rock_positions = {i+gas_direction: height for i, height in rock_positions.items() if height is not None}
-            for i in slot_to_height:
-                if i not in next_rock_positions:
-                    next_rock_positions[i] = None
-            if any(invalid_rock_position(i, height, slot_to_height) for i, height in next_rock_positions.items()):
+            num_gas_changes += 1
+            if gas_direction == 1 and rock_positions[6] is not None:
+                pass
+            elif gas_direction == -1 and rock_positions[0] is not None:
                 pass
             else:
-                left += gas_direction
-                rock_positions = next_rock_positions
-        # print(list(rock_positions[i] for i in sorted(slot_to_height)))
+                next_rock_positions = next_positions_side(rock_positions, gas_direction)
+                if intersects_with_other_rock(slot_heights, next_rock_positions):
+                    pass
+                else:
+                    rock_positions = next_rock_positions
+        
+        def get_move_str(down, gas_direction):
+            if down:
+                return 'down'
+            return 'left' if gas_direction == -1 else 'right'
+        # print_game(slot_heights, rock_positions, get_move_str(down, gas_direction))
+        # import pdb; pdb.set_trace()
         down = False if down else True
 
-    # Adjust slot to height with new rock
-    
-    # for i in slot_to_height:
-    #     import pdb; pdb.set_trace()
-    #     current_height = slot_to_height[i]
-    #     rock_height = max(rock.get(i-left)) if rock.get(i-left, []) else None
-    #     print(i, rock_height)
-    #     if rock_height:
-    #         slot_to_height[i] = current_height + rock_height
-    to_add = {}
-    for height, i_with_this_height in rock.items():
-        for i in i_with_this_height:
-            to_add[left + i] = height+1
-    for i, height in to_add.items():
-        slot_to_height[i] += height
 
-    # import pdb; pdb.set_trace()
-    return slot_to_height, gas_directions
+    # Adjust slot heights with new rock
+    for i, positions in rock_positions.items():
+        if positions is not None:
+            for height in positions:
+                slot_heights[i].append(height)
+
+    # Cap min vals for slot height at floor of game
+    min_height = min(min(j for j in i if j != -1) if len(i) > 1 else -1 for i in slot_heights.values())
+    for i in slot_heights:
+        slot_heights[i] = [h for h in slot_heights[i] if h > min_height-1]
+
+    return slot_heights, gas_directions, num_gas_changes
 
 
-def starting_rock_position(rock, max_height, slot_to_height):
-    result = {i: None for i in slot_to_height}
-    for i in rock[0]:
-        result[i+2] = max_height + 3
+def print_game(slot_to_height, rock_positions, last_move):
+    min_height = min(min(i) for i in slot_to_height.values())
+    max_height = max(max(i) for i in rock_positions.values() if i is not None)
+    print('.|.......|')
+    for i in range(max_height, min_height, -1):
+        row = []
+        row.append(str(i))
+        row.append('|')
+        for col in slot_to_height:
+            if i in slot_to_height[col]:
+                row.append('X')
+            elif rock_positions[col] is not None and i in range(rock_positions[col][0], rock_positions[col][-1]+1):
+                row.append('@')
+            else:
+                row.append('.')
+        row.append('|')
+        print(''.join(row))
+    print('.|.......|')
+    print(last_move, max(max(v) for v in slot_to_height.values()))
+
+
+def intersects_with_other_rock(slot_heights, rock_positions):
+    for i, positions in rock_positions.items():
+        if positions is None:
+            continue
+        rock_at_i = set(range(positions[0], positions[-1]+1))
+        slot_at_i = set(slot_heights[i])
+        if rock_at_i.intersection(slot_at_i):
+            return True
+    return False
+
+def next_positions_down(rock_positions):
+    result = {}
+    for i, positions in rock_positions.items():
+        if positions is None:
+            result[i] = None
+        else:
+            result[i] = [p-1 for p in positions]
+    return result
+
+
+def next_positions_side(rock_positions, gas_direction):
+    result = {i: None for i in rock_positions}
+    for i, positions in rock_positions.items():
+        if positions is not None:
+            result[i+gas_direction] = positions
+    return result
+
+
+def starting_rock_position(rock, max_height, slot_heights):
+    max_height = max(max(v) for v in slot_heights.values())
+    result = {i: None for i in slot_heights}
+    for i, positions in rock.items():
+        result[i+2] = [max_height + p + 3 + 1 for p in positions] if positions else None
     return result
 
 
@@ -131,10 +194,6 @@ def rock_is_stopped(rock, rock_bottom_left, slot_to_height):
     return True
 
 
-def run_2(inputs):
-    pass
-
-
 def run_tests():
     test_inputs = """
     >>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
@@ -144,9 +203,9 @@ def run_tests():
     if result_1 != 3068:
         raise Exception(f"Test 1 did not pass, got {result_1}")
 
-    # result_2 = run_2(test_inputs)
-    # if result_2 != 0:
-    #     raise Exception(f"Test 2 did not pass, got {result_2}")
+    result_2 = run_2(test_inputs)
+    if result_2 != 1514285714288:
+        raise Exception(f"Test 2 did not pass, got {result_2}")
 
 
 if __name__ == "__main__":
@@ -157,5 +216,5 @@ if __name__ == "__main__":
     result_1 = run_1(input)
     print(f"Finished 1 with result {result_1}")
 
-    # result_2 = run_2(input)
-    # print(f"Finished 2 with result {result_2}")
+    result_2 = run_2(input)
+    print(f"Finished 2 with result {result_2}")
